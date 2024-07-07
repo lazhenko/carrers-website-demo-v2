c1be0a625c691e75a1cff29a124631e5d3dbf407
Artem Lazhenko <55236024+lazhenko@users.noreply.github.com>
2024-07-07 15:04:32 +0000

    store application in database

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine,text
 import os
-
+from sqlalchemy import create_engine,text
+from sqlalchemy.exc import SQLAlchemyError
+from werkzeug.datastructures import ImmutableMultiDict
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
@@ -25,3 +26,23 @@ def load_job_from_db(id):
 			return None
 		else:
 			return rows[0]._asdict()
+
+
+def add_application_to_db(job_id:int, data: ImmutableMultiDict):
+	try:
+			with engine.begin() as conn:
+					query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+					query_params = {
+							'job_id': job_id,
+							'full_name': data['full_name'],
+							'email': data['email'],
+							'linkedin_url': data['linkedin_url'],
+							'education': data['education'],
+							'work_experience': data['work_experience'],
+							'resume_url': data['resume_url']
+					}
+					result = conn.execute(query, query_params)
+					return result.rowcount > 0
+	except SQLAlchemyError as e:
+			print(f"An error occurred: {e}")
+			return False
